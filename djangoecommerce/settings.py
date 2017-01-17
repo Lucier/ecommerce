e39b5ed4d9c4228c785cbc154f74e8a6ab40cd92
Lucier Lima <lucierflima@gmail.com>
2017-01-17 00:59:41 -0300

    teste

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
@@ -129,8 +130,6 @@ DATABASES['default'].update(db_from_env)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT = 'static'
 
 try:
     from local_settings import *
