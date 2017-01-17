e641d5d5ba4f58bef8a56989f752ac0299e13aa2
Lucier Lima <lucierflima@gmail.com>
2017-01-17 01:31:18 -0300

    wsgi

--- djangoecommerce/wsgi.py
+++ djangoecommerce/wsgi.py
@@ -1,17 +1,19 @@
 """
-WSGI config for djangoecommerce project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoecommerce.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal.settings")
 
 application = get_wsgi_application()
+
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoecommerce.settings")
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
 
